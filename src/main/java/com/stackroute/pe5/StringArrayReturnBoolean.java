5f2acb8c7ebbc094084d95b4d0f6783ba7241ad8
atul.kumar1 <atul.kumar1@cgi.com>
2019-07-24 14:32:15 +0530

    all the test cases with logic except student class is completed

--- src/main/java/com/stackroute/pe5/StringArrayReturnBoolean.java
+++ src/main/java/com/stackroute/pe5/StringArrayReturnBoolean.java
@@ -1,4 +1,44 @@
 package com.stackroute.pe5;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class StringArrayReturnBoolean {
+    public Map getBooleanManipulation(String[] arr) {
+        if(arr==null)return null;
+        //Converting given string to arraylist
+        List<String> list= Arrays.asList(arr);
+
+        //Creating a HashMap containing char as a key and occurrences as  a value
+        Map<String,Integer> map=new HashMap<>();
+
+        //checking each char of strArray
+        for(String str:list){
+            //If char is present in charCountMap, incrementing it's count by 1
+            if(map.containsKey(str)){
+                map.replace(str,map.get(str)+1);
+            }else{
+                //If char is not present in charCountMap,
+                //putting this char to charCountMap with 1 as it's value
+                map.put(str,1);
+            }
+        }
+
+        // hash map for optput storage
+
+        Map<String,Boolean> booleanMap=new HashMap<>();
+        for(String str:map.keySet()){
+            //if element get count greater than 2 or equal to 2 then add true value to that element
+            if(map.get(str)>=2){
+                booleanMap.put(str,true);
+            }else {
+                //if element get count less than 2 then add false value to that element
+                booleanMap.put(str,false);
+            }
+        }
+        return booleanMap;
+
+    }
 }
--- src/test/java/com/stackroute/pe5/StringArrayReturnBooleanTest.java
+++ src/test/java/com/stackroute/pe5/StringArrayReturnBooleanTest.java
@@ -1,30 +1,57 @@
-//package com.stackroute.pe5;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-//public class StringArrayReturnBooleanTest {
-//    StringArrayReturnBoolean stringArrayReturnBoolean;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        this.stringArrayReturnBoolean=new StringArrayReturnBoolean();
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        this.stringArrayReturnBoolean=null;
-//    }
-//    @Test
-//    public void givenStringArrayShouldReturnTrueFalse(){
-//        String arr[] = {"a","b","c","d","a","c","c"};
-//        String[] expectedResult={"a : true","b :false ","c :true","d : false"};
-//        String[] actualResult=stringArrayReturnBoolean.getBooleanManipulation(arr);
-//        assertArrayEquals(expectedResult,actualResult);
-//
-//    }
-//
-//}
+package com.stackroute.pe5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class StringArrayReturnBooleanTest {
+    StringArrayReturnBoolean stringArrayReturnBoolean;
+
+    @Before
+    public void setUp() throws Exception {
+        this.stringArrayReturnBoolean=new StringArrayReturnBoolean();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.stringArrayReturnBoolean=null;
+    }
+    @Test
+    public void givenStringArrayShouldReturnTrueFalse(){
+        String[] str = {"a","b","c","d","a","c","c"};
+        Map expectedResult = new HashMap<>();
+        expectedResult.put("a", true);
+        expectedResult.put("b", false);
+        expectedResult.put("c", true);
+        expectedResult.put("d", false);
+
+        Map actualResult=stringArrayReturnBoolean.getBooleanManipulation(str);
+        assertEquals(expectedResult,actualResult);
+
+    }
+    @Test
+    public void givenAnotherStringArrayShouldReturnTrueFalse(){
+        String[] str = {"a","b","c","d","a","c","c","b","d"};
+        Map expectedResult = new HashMap<>();
+        expectedResult.put("a", true);
+        expectedResult.put("b", true);
+        expectedResult.put("c", true);
+        expectedResult.put("d", true);
+
+        Map actualResult=stringArrayReturnBoolean.getBooleanManipulation(str);
+        assertEquals(expectedResult,actualResult);
+
+    }
+    @Test
+    public void givenNullValueShouldReturnNull(){
+        Map expectedResult = null;
+        Map actualResult=stringArrayReturnBoolean.getBooleanManipulation(null);
+        assertEquals(expectedResult,actualResult);
+    }
+
+}
